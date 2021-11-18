da761482e546e7922c8de4261ac543d9dc0b6c8e
Nicholas Silva Ruiz <bnsilva5@misena.edu.co>
2021-11-18 11:40:03 -0500

    Modulos e importacion de modulos

---
+++ CursoPython/Fase 4 - Temas avanzados/Tema 11 - Modulos/Leccion 01 - Modulos/saludos.py
@@ -0,0 +1,6 @@
+def saludar():
+    print("Hi, Saludando desde la funcion saludar del modulo saludos")
+
+class Saludo():
+    def __init__(self):
+        print("Hello, saludando desde el init de la clase") 
