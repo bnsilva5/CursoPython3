83b5cb6e48146f026f0ce2b00909b05dfa62d99e
Nicholas Silva Ruiz <bnsilva5@misena.edu.co>
2022-03-14 12:05:03 -0500

    Creacion de radiobuttons

---
+++ CursoPython/Fase 4 - Temas avanzados/Tema 13 - Interfaces graficas con tkinter/radiobuttons.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+def seleccionar():
+    monitor.config(text="{}".format(opcion.get()))
+
+def reset():
+    opcion.set(None)
+    monitor.config(text="")
+
+root = Tk()
+
+opcion = IntVar()
+                                                        # Command se llama la funcion
+Radiobutton(root, text="Opcion 1", variable=opcion, value=1, command=seleccionar).pack()
+Radiobutton(root, text="Opcion 2", variable=opcion, value=2, command=seleccionar).pack()
+Radiobutton(root, text="Opcion 3", variable=opcion, value=3, command=seleccionar).pack()
+
+monitor = Label(root)
+monitor.pack()
+
+Button(root, text="Reiniciar", command=reset).pack()
+
+# Abajo de todo
+root.mainloop()
